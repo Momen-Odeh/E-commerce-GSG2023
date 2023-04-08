050e2d13927f9cb76989c180f45dba7f4ab8ed52
Momen-Odeh <92532348+Momen-Odeh@users.noreply.github.com>
2023-04-08 14:07:53 +0300

    categoryItem done

--- src/Router/index.js
+++ src/Router/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Layout from '../Components/Layout'
+import CategoryItem from '../Components/CategoryItem'
+import CategoryList from '../Components/CategoryList'
 function Router() {
   return (
     <BrowserRouter>
         <Routes>
             <Route path='/' element={<Layout/>}>
-              <Route index element={<h1>this is main route</h1>}/>
+              <Route index element={<CategoryList/>}/>
               <Route path='/homePage' element={<h1>this is home page route</h1>}/>
               <Route path='*' element={<h1>404 Not Found</h1>}/>  
             </Route>
