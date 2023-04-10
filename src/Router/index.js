7488355428cf137c6656f03f5fd0e70f09cefd58
Momen-Odeh <92532348+Momen-Odeh@users.noreply.github.com>
2023-04-10 19:53:40 +0300

    create home page

--- src/Router/index.js
+++ src/Router/index.js
@@ -3,12 +3,13 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Layout from '../Components/Layout'
 import CategoryItem from '../Components/CategoryItem'
 import CategoryList from '../Components/CategoryList'
+import HomePage from '../Components/HomePage'
 function Router() {
   return (
     <BrowserRouter>
         <Routes>
             <Route path='/' element={<Layout/>}>
-              <Route index element={<CategoryList/>}/>
+              <Route index element={<HomePage/>}/>
               <Route path='/homePage' element={<h1>this is home page route</h1>}/>
               <Route path='*' element={<h1>404 Not Found</h1>}/>  
             </Route>
