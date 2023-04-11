28f9e2bff4832f9e6c2835c9cb68860ffd6fc663
Momen-Odeh <92532348+Momen-Odeh@users.noreply.github.com>
2023-04-11 19:02:21 +0300

    start work on Product Container

---
+++ src/Components/ProductContainerLeft/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import stayles from './index.module.css'
+function ProductContainerLeft() {
+return (
+    <div className={stayles.ProductContainerLeft}>ProductContainerLeft</div>
+  )
+}
+
+export default ProductContainerLeft
---
+++ src/Components/ProductContainerRight/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import stayles from './index.module.css'
+function ProductContainerRight() {
+  return (
+
+    <div className={stayles.ProductContainerRight}>ProductContainerRight</div>
+  )
+}
+
+
+export default ProductContainerRight
