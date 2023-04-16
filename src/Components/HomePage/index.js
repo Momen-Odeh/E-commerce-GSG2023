121c1916ed946ac707b3fba25af1fa6e7766be0c
Momen-Odeh <92532348+Momen-Odeh@users.noreply.github.com>
2023-04-16 23:56:26 +0300

    Product card by mahmoud

--- src/Components/HomePage/index.js
+++ src/Components/HomePage/index.js
@@ -2,13 +2,16 @@ import React from 'react'
 import CategoryList from '../CategoryList'
 import MusicExperience from '../MusicExperience'
 import HR from '../HR'
-
+import ProductCard from '../ProductCard'
+import PS from '../ProductCard/PS.png'
 function HomePage() {
   return (
     <div className='HomePage'>
         <CategoryList/>
         <HR/>
         <MusicExperience/>
+        <HR/>
+        <ProductCard image={PS} title="title" rating={2} price={200} />
     </div>
   )
 }
