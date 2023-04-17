8401df61a18587408f08c2b2893befda755ddbcf
Moath <m_darkknight@hotmail.com>
2023-04-17 13:03:33 +0300

    Fixed the Appstore and Googleplay images.

--- src/Components/HomePage/index.js
+++ src/Components/HomePage/index.js
@@ -4,18 +4,16 @@ import MusicExperience from '../MusicExperience'
 import HR from '../HR'
 import Arrivals from '../Arrivals'
 import Cards from '../Services'
-
 function HomePage() {
-  return (
-    <div className='HomePage'>
-        <CategoryList/>
-        <HR/>
-        <MusicExperience/>
-        <Arrivals />
-        <Cards />
-      
-    </div>
-  )
+        return (
+                <div className='HomePage'>
+                        <CategoryList />
+                        <HR />
+                        <MusicExperience />
+                        <Arrivals />
+                        <Cards />
+                </div>
+        )
 }
 
 export default HomePage
