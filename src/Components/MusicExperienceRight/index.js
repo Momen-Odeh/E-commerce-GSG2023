8665f551ccd92246ae9bf711a5a414db6a593834
Momen-Odeh <92532348+Momen-Odeh@users.noreply.github.com>
2023-04-19 02:36:00 +0300

    fix music image

--- src/Components/MusicExperienceRight/index.js
+++ src/Components/MusicExperienceRight/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './index.module.css'
-import Speaker from './images/JBL.png'
+import Speaker from './images/jbl.png'
 function MusicExperienceRight() {
   return (
     <div className={styles.MusicExperienceRight}>
