2ff15d71a207c3d1140e528ac98a292741df9cea
Momen-Odeh <92532348+Momen-Odeh@users.noreply.github.com>
2023-04-20 00:37:58 +0300

    refactor product images

--- src/Components/ProductContainer/index.js
+++ src/Components/ProductContainer/index.js
@@ -5,15 +5,16 @@ import ProductContainerRight from '../ProductContainerRight'
 import { useParams } from 'react-router-dom';
 import {data } from '../ProductPreview/data'
 import {data2} from '../ProductPreview/data2'
+import Grid from '@mui/material/Grid';
 function ProductContainer() {
   const {id} = useParams();
   let tmp = data.filter(x => x.title ===id )
-  tmp.length === 0 ?tmp = data2.filter(x => x.title ===id ):console.log("dd",tmp);
+  tmp.length === 0 ?tmp = data2.filter(x => x.title ===id ):console.log("get data",tmp);
   return (
-    <div className={stayles.ProductContainer}>
-      <ProductContainerLeft data={tmp[0]}/>
-      <ProductContainerRight  data={tmp[0]}/>
-    </div>
+    <Grid container width={"81.25%"} rowGap={'30px'} justifyContent={'space-between'} margin={"0 auto"} marginBottom={'100px'}>
+      <Grid item lg={7.5} md={9} sm={12}><ProductContainerLeft data={tmp[0]}/></Grid>
+      <Grid item lg={4} md={2.5} sm={12}><ProductContainerRight  data={tmp[0]}/></Grid>
+    </Grid>
     
   )
 }
--- src/Components/ProductContainerLeft/index.js
+++ src/Components/ProductContainerLeft/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 import stayles from './index.module.css'
 import ProductContainerLeftImages from '../ProductContainerLeftImages'
 import ProductContainerRightImage from '../ProductContainerRightImage'
+import Grid from '@mui/material/Grid';
 function ProductContainerLeft({data}) {
 return (
-    <div className={stayles.ProductContainerLeft}>
-      <ProductContainerLeftImages imgs={data.leftImg}/>
-      <ProductContainerRightImage img ={data.img}/>
-    </div>
+    <Grid container className={stayles.ProductContainerLeft} justifyContent={'space-between'} rowGap={'30px'} >
+      <Grid item lg={4} md={4.3} sm={4.5} xs={12} margin={'0 auto'}><ProductContainerLeftImages imgs={data.leftImg}/></Grid>
+      <Grid item lg={7.4} md={7.3} sm={7} xs={12}><ProductContainerRightImage img ={data.img}/></Grid>
+    </Grid>
   )
 }
 
